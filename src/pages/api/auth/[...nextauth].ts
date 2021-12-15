537d2c55870ce5ed7e2b854628195748775b5f4a
Matheus Teixeira <matheusctx@pop-os.localdomain>
2021-12-15 14:25:41 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GitHubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -12,5 +15,23 @@ export default NextAuth({
         },
       },
     }),
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+        
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
