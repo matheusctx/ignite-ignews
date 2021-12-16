1c31c57cc45a170be6d259a3bcf7dc262a775fd3
Matheus Teixeira <matheusctx@pop-os.localdomain>
2021-12-16 12:12:15 -0300

    Componente: ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { SignInButton } from '../SignInButton';
-import Link from 'next/link';
 
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -9,13 +9,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
 
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
