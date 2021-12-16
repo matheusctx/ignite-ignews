914835f8b24090ce8db8a7333392fe5edc937349
Matheus Teixeira <matheusctx@pop-os.localdomain>
2021-12-16 11:40:13 -0300

    Consumindo API do Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from '@prismicio/client';
 
 import styles from './styles.module.scss';
 
@@ -11,19 +14,19 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
+          <a href="#">
             <time>15 de dezembro de 2021</time>
             <strong>Create a Monorepo</strong>
             <p>In this guide, you will learn how to create a Monorepo</p>
           </a>
 
-          <a>
+          <a href="#">
             <time>15 de dezembro de 2021</time>
             <strong>Create a Monorepo</strong>
             <p>In this guide, you will learn how to create a Monorepo</p>
           </a>
 
-          <a>
+          <a href="#">
             <time>15 de dezembro de 2021</time>
             <strong>Create a Monorepo</strong>
             <p>In this guide, you will learn how to create a Monorepo</p>
@@ -32,4 +35,21 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  });
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {}
+  }
 }
